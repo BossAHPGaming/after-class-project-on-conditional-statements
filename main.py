37b9299c02d310bef9043102ae054d3f57d102a8
BossAHPGaming <aaravhpatel210@gmail.com>
2024-12-02 18:46:46 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+number = int(input("Enter your attendance: "))
+print(number)
+
+if number>75:
+    print("Your are allowed to write the boards")
+else:
+    print("Your not are allowed to write the boards")
